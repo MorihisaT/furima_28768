9bc998d4ee4470a30509ae65e03f8280202dc299
MorihisaT <morihisa87go@gmail.com>
2020-08-24 16:51:23 +0900

    Revert "バリデーションの設定"
    This reverts commit 364bb51fe1883c199e1615b29ad2ab8b20f3520f.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  with_option presence: true do
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥], message: "is invalid. Input full_width characters."}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥], message: "is invalid. Input full_width characters."}
-    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full_width characters."}
-    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full_width characters."}
-    validates :birthday
-    validates :nickname
-    validates :email, format: { with: /@.+/ }, uniqueness: true
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i、message: "is invalid. Input include both letters and numbers"}, length: {minimum: 6}, confirmation: true
-    validates :password_confirmation
-  end
+
 end
