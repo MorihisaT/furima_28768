db9ba2a99b6d4a52d90e343f77070e0592b151dd
MorihisaT <morihisa87go@gmail.com>
2020-08-24 17:58:38 +0900

    バリデーションの修正＃2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   with_options presence: true do
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full_width characters."}
-    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full_width characters."}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "full-width characters."}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "full-width characters."}
+    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "full_width characters."}
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/}
     validates :birthday
     validates :nickname
-    validates :email, format: { with: /@.+/ }, uniqueness: true
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input include both letters and numbers"}, length: {minimum: 6}, confirmation: true
-    validates :password_confirmation
   end
+  validates :email, format: { with: /@.+/ }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "include both letters and numbers"}, length: {minimum: 6}
 end
