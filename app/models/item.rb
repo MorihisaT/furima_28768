f1c20b322fec118d4ea532401400cb270920647e
MorihisaT <morihisa87go@gmail.com>
2020-08-25 19:09:11 +0900

    バリデーション及びアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordEctensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :area
+  belongs_to_active_hash :delivery_time
+
+  belongs_to :user
+
+  validates :image, :name, :discript, :category, :status, :delivery_fee, :area, :delivery_time, presence: true
+  validates :category_id, :status_id, :delivery_fee_id, :area_id, :delivery_time_id, numericality: { other_than: 1 }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }, presence: true
 end
