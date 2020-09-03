8af9c731138cdb98f0cd6581d3dc1d6b5b2e6d01
MorihisaT <morihisa87go@gmail.com>
2020-09-03 15:51:33 +0900

    formオブジェクトの修正

--- app/forms/address_order.rb
+++ app/forms/address_order.rb
@@ -1,6 +1,6 @@
 class AddressOrder
   include ActiveModel::Model
-  attr_accessor :postal_code, :area_id, :city, :house_number, :building_name, :phone_number, :item_id, :user_id
+  attr_accessor :postal_code, :area_id, :city, :house_number, :building_name, :phone_number, :item_id, :user_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'indlude hyphen(-)' }
@@ -8,9 +8,9 @@ class AddressOrder
     validates :phone_number, format: { with: /\A[0-9]+\z/, message: 'is not include hyphen(-)' }, length: { maximum: 11 }
   end
   validates :city, :house_number, presence: true
-
+  validates :token, presence: true
   def save
-    order = Order.create(item_id: params[:item_id], user_id: current_user.id)
-    Address.create(postal_code: params[postal_code], area_id: params[area_id], city: params[:city], house_number: params[:house_number], building_name: params[:building_name], phone_number: params[:phone_number], order_id: order.id)
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, area_id: area_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
