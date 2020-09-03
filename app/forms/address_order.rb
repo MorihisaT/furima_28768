89dba48a52bed6ac082001cdaf6e00116cd29fa2
MorihisaT <morihisa87go@gmail.com>
2020-09-03 17:20:33 +0900

    バリデーションの修正

--- app/forms/address_order.rb
+++ app/forms/address_order.rb
@@ -6,9 +6,9 @@ class AddressOrder
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'indlude hyphen(-)' }
     validates :area_id, numericality: { other_than: 1 }
     validates :phone_number, format: { with: /\A[0-9]+\z/, message: 'is not include hyphen(-)' }, length: { maximum: 11 }
+    validates :city, :house_number, :token
   end
-  validates :city, :house_number, presence: true
-  validates :token, presence: true
+  
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
     Address.create(postal_code: postal_code, area_id: area_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
